872b4042a6e3fa482578cfa8d78dcdd76ade4f9d
Milky <69197339+Doonort3@users.noreply.github.com>
2022-10-19 14:26:30 +0300

    Add files via upload

---
+++ undefined.cc
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "Ты определяешь реальность, а не реальность определяет тебя." << endl;
+    cout << "You define reality, not reality defines you." << endl;  
+    cout << "Вие определяте реалността, а не тя определя вас." << endl;
+    cout << "Te határozod meg a valóságot, nem a valóság határoz meg téged." << endl;
+    cout << "Εσείς ορίζετε την πραγματικότητα, όχι η πραγματικότητα εσάς." << endl;
+    cout << "Det er dig, der definerer virkeligheden, ikke virkeligheden, der definerer dig." << endl;
+    cout << "Anda mendefinisikan realitas, bukan realitas yang mendefinisikan Anda." << endl;
+    cout << "Tú defines la realidad, no la realidad te define a ti." << endl;
+    cout << "Siete voi a definire la realtà, non la realtà a definire voi." << endl;
+    cout << "Jūs definējat realitāti, nevis realitāte definē jūs." << endl;
+    cout << "Jūs apibrėžiate realybę, o ne realybė apibrėžia jus." << endl;
+    cout << "Sie definieren die Realität, nicht die Realität Sie." << endl;
+    cout << "Jij bepaalt de realiteit, niet de realiteit bepaalt jou." << endl;
+    cout << "To ty definiujesz rzeczywistość, a nie rzeczywistość definiuje ciebie." << endl;
+    cout << "Você define a realidade, não a realidade o define." << endl;
+    cout << "Tu definești realitatea, nu realitatea te definește pe tine." << endl;
+    cout << "Vy definujete realitu, nie realita definuje vás." << endl;
+    cout << "Vi določate resničnost, ne resničnost določa vas." << endl;
+    cout << "Gerçekliği siz tanımlarsınız, gerçeklik sizi değil." << endl;
+    cout << "Ти визначаєш реальність, а не реальність визначає тебе." << endl;
+    cout << "Sinä määrittelet todellisuuden, ei todellisuus määrittele sinua." << endl;
+    cout << "C'est vous qui définissez la réalité, et non la réalité qui vous définit." << endl;
+    cout << "Vy určujete realitu, ne realita určuje vás." << endl;
+    cout << "Du definierar verkligheten, inte verkligheten definierar dig." << endl;
+    cout << "Teie määratlete reaalsust, mitte reaalsus ei määratle teid." << endl;
+    cout << "現実を定義するのはあなたであり、現実があなたを定義するのではありません。。。" << endl;
+    
+}
